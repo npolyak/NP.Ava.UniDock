4dd08d1fea25f420f92e13d9c6ba07b222076c43
npolyak <nick.polyak@verizon.net>
2024-01-01 18:39:17 -0500

    fixed solution

--- Prototypes/NP.DataContextSample/MyCustomFloatingWindowFactory.cs
+++ Prototypes/NP.DataContextSample/MyCustomFloatingWindowFactory.cs
@@ -1,6 +1,6 @@
 ﻿using Avalonia.Controls;
-using NP.Avalonia.UniDock;
-using NP.Avalonia.UniDock.Factories;
+using NP.Ava.UniDock;
+using NP.Ava.UniDock.Factories;
 
 namespace NP.DataContextSample
 {
@@ -12,7 +12,7 @@ namespace NP.DataContextSample
 
             FloatingWindow dockWindow = new FloatingWindow();
 
-            dockWindow.Classes = new Classes("PlainFloatingWindow", "MyFloatingWindow");
+            dockWindow.Classes.AddRange(new []{ "PlainFloatingWindow", "MyFloatingWindow"});
             dockWindow.TitleClasses = "WindowTitle";
             dockWindow.IsDockWindow = true;
 
