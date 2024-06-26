9f5bb286c6e1eaa21f4fd1f7da3bb4d2b4ac313b
nick <nick.polyak@verizon.net>
2024-06-26 16:53:25 -0400

    checkpoint

---
+++ Prototypes/NP.PanelDraggingSample/MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+
+namespace NP.PanelDraggingSample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+    }
+}
