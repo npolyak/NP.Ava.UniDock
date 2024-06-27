7f6637ef0a5c5fafe3870052d1ad8e4fba7073b9
npolyak <nick.polyak@verizon.net>
2024-06-27 13:15:39 -0400

    created initial project for mac dragging tests

---
+++ Prototypes/NP.DragOutForMacTest/MainWindow.axaml.cs
@@ -0,0 +1,19 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Avalonia.VisualTree;
+using NP.Ava.UniDock;
+using System.Linq;
+
+namespace NP.DragOutForMacTest
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
