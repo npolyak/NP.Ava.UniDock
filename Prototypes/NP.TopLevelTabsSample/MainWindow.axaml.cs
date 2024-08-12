83cff797b5b00ccc5316941e32e5bcd21536bfed
npolyak <nick.polyak@verizon.net>
2024-08-12 16:04:51 -0400

    created testing project

---
+++ Prototypes/NP.TopLevelTabsSample/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia;
+using Avalonia.Controls;
+namespace NP.TopLevelTabsSample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
